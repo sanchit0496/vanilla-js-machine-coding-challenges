8d507c8ed08b3dfae8f9e5be95b14c7bbc548b01
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-05 18:17:29 +0530

    modal with logic complete

---
+++ modal-popup/script.js
@@ -0,0 +1,7 @@
+const handleOpenModal = () => {
+    document.querySelector('.modal').style.display = 'block'
+}
+
+const handleCloseModal = () => {
+    document.querySelector('.modal').style.display = 'none'
+}

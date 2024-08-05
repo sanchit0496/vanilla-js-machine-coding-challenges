cb8cde24e50d0f581dd88b2feea0e740d3a0926c
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-05 20:00:46 +0530

    added reusable function to add task here

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,7 +1,17 @@
-let taskList = [];
+const taskHolder = document.getElementById("task-holder");
 
 document.getElementById("add-task-button").addEventListener("click", () => {
-
   const inputComponent = document.getElementById("input-component");
-  console.log(inputComponent.value);
+  if (inputComponent.value.length === 0) {
+    alert("Value cannot be empty");
+  }
+  addTask(inputComponent.value);
+  inputComponent.value = "";
 });
+
+const addTask = (taskInput) => {
+  const taaskItem = document.createElement("div");
+  taaskItem.textContent = taskInput;
+
+  taskHolder.appendChild(taaskItem);
+};

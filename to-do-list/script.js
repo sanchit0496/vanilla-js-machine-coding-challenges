f081e3bcc1a1ab7fa817ce8987162c3b04b99cff
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-05 19:51:39 +0530

    have added basic code

---
+++ to-do-list/script.js
@@ -0,0 +1,7 @@
+let taskList = [];
+
+document.getElementById("add-task-button").addEventListener("click", () => {
+
+  const inputComponent = document.getElementById("input-component");
+  console.log(inputComponent.value);
+});

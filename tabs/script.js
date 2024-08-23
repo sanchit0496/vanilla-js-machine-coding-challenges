54d49057194e8c5568f4f97587fc4ea823c1640b
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-23 15:01:33 +0530

    have completed tab application

--- tabs/script.js
+++ tabs/script.js
@@ -1,12 +1,30 @@
+const TAB_COUNT = 5
 const tabContainer = document.getElementById('tab-container')
 const contentContainer = document.getElementById('content-container')
 
-const tabArray =  new Array(5).fill().map((_, index) => index+1)
+const tabArray =  new Array(TAB_COUNT).fill().map((_, index) => index)
+const descriptionArray = new Array(TAB_COUNT).fill().map((_, index) => {
+  return `Content of ${index}`
+})
 
-tabArray.forEach((index) => {
+let activeTab = 0
+contentContainer.textContent = descriptionArray[activeTab]
+
+tabArray.forEach((item, index) => {
   const tabDiv = document.createElement('div')
-  tabDiv.classList.add('tab')
-  tabDiv.textContent = `Tab ${index}`
+  index === 0 ? tabDiv.classList.add('tab', 'active') : tabDiv.classList.add('tab')
+  tabDiv.textContent = `Tab ${item}`
 
+  tabDiv.addEventListener('click', () => {
+    let divs = document.querySelectorAll('.tab')
+    divs.forEach((item) => item.classList.remove('active'))
+    tabDiv.classList.add('active')
+    activeTab = index
+
+    contentContainer.textContent = descriptionArray[activeTab]
+
+  })
   tabContainer.appendChild(tabDiv)
 })
+
+

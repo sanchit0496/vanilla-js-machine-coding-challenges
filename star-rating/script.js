f160755367cd7793ba72e422fbef773fa592e5f9
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-24 20:59:31 +0530

    adding

--- star-rating/script.js
+++ star-rating/script.js
@@ -1,28 +1,35 @@
-const NUMBER_OF_STARS = 5
-const starHolder = document.getElementById('star-holder')
+const NUMBER_OF_STARS = 5;
+const starHolder = document.getElementById("star-holder");
 
 for (let i = 0; i < NUMBER_OF_STARS; i++) {
-    const star = document.createElement('div')
-    star.innerHTML = '*'
-    star.classList.add('star-icon', `${i}`)
+  const star = document.createElement("div");
+  star.innerHTML = "*";
+  star.classList.add("star-icon", `${i}`);
 
-    starHolder.appendChild(star)
+  starHolder.appendChild(star);
 }
 
-const stars = document.querySelectorAll('.star-icon')
+const stars = document.querySelectorAll(".star-icon");
 
-console.log(stars)
+console.log(stars);
+
+const resetStars = () => {
+  for (let i = 0; i < NUMBER_OF_STARS; i++) {
+    stars[i].style.color = "black";
+  }
+};
 
 const updateStar = (index) => {
-    for (let i = 0; i < NUMBER_OF_STARS; i++) {
-        if(i <= index){
-            stars[i].style.color = 'orange'
-        }
+  for (let i = 0; i < NUMBER_OF_STARS; i++) {
+    if (i <= index) {
+      stars[i].style.color = "orange";
     }
-}
+  }
+};
 
 stars.forEach((star, index) => {
-    star.addEventListener('click', () => {
-        updateStar(index)
-    })
-})
+  star.addEventListener("click", () => {
+    resetStars();
+    updateStar(index);
+  });
+});

56f88bf2f12ceffb06fef893063bf6fd9261855b
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-24 17:34:42 +0530

    basic code is complete

---
+++ navbar/script.js
@@ -0,0 +1,43 @@
+var currentWidth = window.innerWidth;
+const navbar = document.querySelector('.navbar')
+const hambHolder = document.getElementById('hamb-holder')
+const handle = document.getElementById('hamburger-handle')
+const hamburgerItem = document.getElementById('hamburger-item')
+
+const hamburgerItems = document.getElementById('hamburger-items')
+const displayNavbar = (currWidth) => {
+    console.log(currWidth)
+    if(currWidth < 460){
+        navbar.style.display = 'none'
+        hambHolder.style.display = 'block'
+
+    }else{   
+        hambHolder.style.display = 'none'
+        navbar.style.display = 'block'
+
+    }
+}
+window.addEventListener('load', () => {
+    currentWidth = window.innerWidth;
+    displayNavbar(currentWidth)
+});
+
+window.addEventListener('resize', () => {
+    currentWidth = window.innerWidth;
+    displayNavbar(currentWidth)
+});
+
+handle.addEventListener('click', ()  => {
+    console.log(hamburgerItems.style.display)
+    hamburgerItems.style.display = 'block'
+
+    // if(hamburgerItems.style.display = 'block'){
+    //     hamburgerItems.style.display = 'none'
+    // }else{
+    //     hamburgerItems.style.display = 'block'
+    // }
+})
+
+hamburgerItem.addEventListener('click', () => {
+    hamburgerItems.style.display = 'none'
+})

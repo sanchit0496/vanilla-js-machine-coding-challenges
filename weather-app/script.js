90c2779cfe59a191647706eca2c1c540dfd8db4b
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-25 00:04:20 +0530

    app complete

--- weather-app/script.js
+++ weather-app/script.js
@@ -4,15 +4,23 @@ const maxTemp = document.getElementById('max-temp')
 const minTemp = document.getElementById('min-temp')
 const humidity = document.getElementById('humidity')
 
-const getWeatherDetail = async () => {
-    const data = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=12&lon=12&appid=bf909b4a90c47e810fc156dc73c0ed75')
+const getWeatherDetail = async (lat, lng) => {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=bf909b4a90c47e810fc156dc73c0ed75`)
     const retData = await data.json()
     return retData
 }
 
+const getLocation = () => {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        }
+    })
+}
+
 (async() => {
-    let weatherDetail = await getWeatherDetail()
-    console.log(weatherDetail)
+    let getLocationDetail = await getLocation()
+    let weatherDetail = await getWeatherDetail(getLocationDetail.coords.latitude, getLocationDetail.coords.longitude)
     city.textContent = `City: ${weatherDetail.name}`
     currentTemp.textContent = `Temp (f): ${weatherDetail.main.temp}`
     maxTemp.textContent = `Max Temp (f): ${weatherDetail.main.temp_max}`

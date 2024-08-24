115387d9c75614f1e71a158d35d5b3f7c3c41a44
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-24 19:31:13 +0530

    have completed the basic logic of app here

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -6,17 +6,17 @@ const timerMin = document.getElementById("timer-min");
 const timerSec = document.getElementById("timer-sec");
 
 const resetStopwatch = () => {
-  timerHour.textContent = '00'
-  timerMin.textContent = '00'
-  timerSec.textContent = '00'
+  timerHour.textContent = "00";
+  timerMin.textContent = "00";
+  timerSec.textContent = "00";
 
-  timerHour.style.color = 'gray'
-  timerMin.style.color = 'gray'
-  timerSec.style.color = 'gray'
-}
-window.addEventListener('load', () => {
-  resetStopwatch()
-})
+  timerHour.style.color = "gray";
+  timerMin.style.color = "gray";
+  timerSec.style.color = "gray";
+};
+window.addEventListener("load", () => {
+  resetStopwatch();
+});
 
 const secInput = document.getElementById("input-sec");
 const secInputErrorSpan = document.getElementById("inputSecError");
@@ -24,35 +24,35 @@ const secInputErrorSpan = document.getElementById("inputSecError");
 let sec = "";
 
 const onlyNumbers = (e) => {
-  let temp = e.target.value.replace(/[^0-9]/g, '')
-  e.target.value = temp
-  return temp
-}
+  let temp = e.target.value.replace(/[^0-9]/g, "");
+  e.target.value = temp;
+  return temp;
+};
 
 secInput.addEventListener("input", (e) => {
-  sec = onlyNumbers(e)
+  sec = onlyNumbers(e);
 });
 
 const setError = (inputElement, errorSpan, errorMessage) => {
   errorSpan.textContent = errorMessage;
-  inputElement.style.border = '1px solid red';
+  inputElement.style.border = "1px solid red";
 };
 
 const removeError = (inputElement, errorSpan) => {
-  errorSpan.textContent = '';
-  inputElement.style.border = '1px solid black';
+  errorSpan.textContent = "";
+  inputElement.style.border = "1px solid black";
 };
 
 const validateForm = () => {
   let errorArr = [];
-  
+
   if (sec.length === 0) {
     setError(secInput, secInputErrorSpan, "Input cannot be empty");
-    errorArr.push('Input cannot be empty');
+    errorArr.push("Input cannot be empty");
   } else {
     removeError(secInput, secInputErrorSpan);
   }
-  
+
   return errorArr;
 };
 
@@ -60,15 +60,22 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   let err = validateForm();
   if (err.length === 0) {
-    console.log('call the API here');
+    console.log("call the API here");
 
-    let minDisplay = Math.floor(sec/ 60)
-    let hourDisplay = Math.floor(sec/ 3600)
-    let secDisplay= sec -(60* minDisplay)
+    setInterval(() => {
+      sec--;
 
-    timerHour.textContent = hourDisplay
-    timerMin.textContent = minDisplay
-    timerSec.textContent = secDisplay
- 
+      let minDisplay = Math.floor(sec / 60);
+      let hourDisplay = Math.floor(sec / 3600);
+      let secDisplay = sec - 60 * minDisplay;
+
+      timerHour.textContent = hourDisplay;
+      timerMin.textContent = minDisplay;
+      timerSec.textContent = secDisplay;
+
+      if (sec === 0) {
+        clearInterval();
+      }
+    }, 1000);
   }
 });

bef22caeb3d5976552924939a6e635d48f7f79e2
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-24 20:57:24 +0530

    basic code is complete

--- star-rating/script.js
+++ star-rating/script.js
@@ -4,9 +4,25 @@ const starHolder = document.getElementById('star-holder')
 for (let i = 0; i < NUMBER_OF_STARS; i++) {
     const star = document.createElement('div')
     star.innerHTML = '*'
-    star.classList.add('star-icon')
+    star.classList.add('star-icon', `${i}`)
 
     starHolder.appendChild(star)
 }
 
+const stars = document.querySelectorAll('.star-icon')
 
+console.log(stars)
+
+const updateStar = (index) => {
+    for (let i = 0; i < NUMBER_OF_STARS; i++) {
+        if(i <= index){
+            stars[i].style.color = 'orange'
+        }
+    }
+}
+
+stars.forEach((star, index) => {
+    star.addEventListener('click', () => {
+        updateStar(index)
+    })
+})

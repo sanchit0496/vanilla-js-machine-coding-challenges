d06de7ab174d53f87f96e71e1fe562d00fb56f8b
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-25 19:19:03 +0530

    adding

--- theme-mode/script.js
+++ theme-mode/script.js
@@ -6,7 +6,31 @@ const desc = document.querySelector(".desc");
 
 toggleBtn.addEventListener("click", () => {
   bodyContainer.classList.toggle("dark");
+
+  if(bodyContainer.classList.contains('dark')){
+    localStorage.setItem('theme', 'dark')
+  }else{
+    localStorage.setItem('theme', 'light')
+  }
+
   card.classList.toggle("dark");
   intro.classList.toggle("dark");
   desc.classList.toggle("dark");
 });
+
+(() => {
+    let currentTheme = localStorage.getItem('theme')   
+    if(currentTheme === null){
+        localStorage.setItem('theme', 'light')
+        bodyContainer.classList.add('light')
+        card.classList.add('light')
+        intro.classList.add('light')
+        desc.classList.add('light')
+    }
+    if(currentTheme === 'dark'){
+        bodyContainer.classList.add('dark')
+        card.classList.add('dark')
+        intro.classList.add('dark')
+        desc.classList.add('dark')
+    }
+})()

4fe9e526a454230911b3d9873a34fbe16dff5569
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-25 17:26:56 +0530

    have added the feature here

--- virtualized-list/script.js
+++ virtualized-list/script.js
@@ -2,7 +2,7 @@
 const TOTAL_ITEMS = 10000;
 const ITEM_HEIGHT = 50;
 const CONTAINER_HEIGHT = window.innerHeight;
-const BUFFER = 5;
+const EXTRA_BUFFER_ITEMS = 5;
 
 // DOM Elements
 const listContainer = document.getElementById("list-container");
@@ -12,13 +12,25 @@ const spacer = document.getElementById("spacer");
 spacer.style.height = `${TOTAL_ITEMS * ITEM_HEIGHT}px`;
 
 const createList = () => {
-  for (let i = 0; i < 20; i++) {
+  let scrollTop = listContainer.scrollTop;
+  let startNode = Math.floor(scrollTop / ITEM_HEIGHT) - EXTRA_BUFFER_ITEMS;
+  let visibleNodes =
+    Math.ceil(CONTAINER_HEIGHT / ITEM_HEIGHT) + EXTRA_BUFFER_ITEMS;
+
+  let startIndex = Math.max(0, startNode);
+  let endIndex = Math.min(startIndex + visibleNodes, TOTAL_ITEMS);
+
+  list.innerHTML = "";
+
+  for (let i = startIndex; i < endIndex; i++) {
     const itemDiv = document.createElement("div");
     itemDiv.classList.add("item");
     itemDiv.textContent = `Div ${i}`;
     itemDiv.style.top = `${i * ITEM_HEIGHT}px`;
+    itemDiv.style.height = `${ITEM_HEIGHT}px`;
     list.appendChild(itemDiv);
   }
+
   listContainer.appendChild(list);
 };
 

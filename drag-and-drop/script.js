ac4ee323914a0d907a98e1fd8ddae3ce2ec31418
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-25 13:03:03 +0530

    adding

--- drag-and-drop/script.js
+++ drag-and-drop/script.js
@@ -1,13 +1,36 @@
-const itemHolder = document.getElementById('item-holder')
-const NUMBER_OPTIONS = 8
+const itemHolder = document.getElementById('item-holder');
+const NUMBER_OPTIONS = 8;
 
-const items = new Array(NUMBER_OPTIONS).fill().map((_, index) => index)
-console.log(items)
+// Create items
+const items = new Array(NUMBER_OPTIONS).fill().map((_, index) => index);
+
+let startDivId;
 
 items.forEach((item) => {
-    const itemDiv = document.createElement('div')
-    itemDiv.classList.add('item')
-    itemDiv.draggable = true
-    itemDiv.textContent = item
-    itemHolder.appendChild(itemDiv)
-})
+    const itemDiv = document.createElement('div');
+    itemDiv.classList.add('item');
+    itemDiv.draggable = true;
+    itemDiv.textContent = `Item ${item}`;
+    itemDiv.id = `item-${item}`;
+    itemDiv.classList.add(`${item}`)
+
+    // Store the ID of the dragged element
+    itemDiv.addEventListener('dragstart', (e) => {
+        startDivId = e.target.id
+    });
+
+    // Handle the drop event
+    itemDiv.addEventListener('dragover', (e) => {
+        e.preventDefault(); 
+    });
+
+    itemDiv.addEventListener('drop', (e) => {
+        e.preventDefault();
+        const draggedElement = document.getElementById(startDivId);
+        if(startDivId !== e.target.id){
+            itemHolder.insertBefore(draggedElement, e.target);
+        }
+    });
+
+    itemHolder.appendChild(itemDiv);
+});
